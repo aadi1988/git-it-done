4ecb8038fed6dad8a980304633ba12db8c91f163
Aaditi <aadi2007.aaditi@gmail.com>
2020-08-15 21:49:16 -0700

    initial file

---
+++ assets/js/homepage.js
@@ -0,0 +1,13 @@
+var user = "facebook"
+var getUserRepos = function() {
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+       response.json().then(function(data) {
+          console.log(data);
+    });
+  });
+};
+  
+  getUserRepos();
